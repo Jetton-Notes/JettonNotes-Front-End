e857a982ff1d8835735eabf2d09268cd2b58b8d9
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-02-14 22:06:35 +0100

    added constants

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export const appName = "Jetton Notes"
+
+export const website = "https://jettonnotes.com"
+
+export const botlink = "https://t.me/jetton_notes_bot"
+
+export const tgBTC_jetton_master = "JETTON MASTER ADDR PALCEHOLDER"
+
+export const currentNetwork = "TON testnet"
+
+export const onlyCURR = "tgBTC";
