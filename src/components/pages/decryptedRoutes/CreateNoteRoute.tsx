96b6932e31814bac23d31128a6118ffb39f16801
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-02-17 15:26:19 +0100

    updated navigation

--- src/components/pages/decryptedRoutes/CreateNoteRoute.tsx
+++ src/components/pages/decryptedRoutes/CreateNoteRoute.tsx
@@ -2,14 +2,11 @@ import { Button, Box, Typography, Paper, TextField, Stack } from "@mui/material"
 import { RouteFooter } from "../../Footer";
 
 export type CreateRouteProps = {
-    depositAmount: string,
-    setDepositAmount: (to: string) => void,
     onNotify: (msg: string, type: string) => void,
-    createNoteClicked: (amount: string) => Promise<void>
+    createNoteClicked: () => Promise<void>
 }
 
 
-//THen when it's been copied the user can make the deposit
 export function CreateRoute(props: CreateRouteProps) {
     return <Box >
         <Paper sx={{ maxWidth: 936, margin: "auto", overflow: "hidden", mt: "10px" }}>
@@ -17,17 +14,10 @@ export function CreateRoute(props: CreateRouteProps) {
                 <Typography component="h1" variant="h4">Jetton Notes</Typography>
             </Stack>
             <Stack sx={{ padding: "30px" }} direction={"row"} justifyContent="center">
-                <Typography component="p" variant="subtitle1">Jetton Notes are financial claims for value that was deposited into a smart contract. Currently available for tgBTC. Enter the denomination, download the printable note and make a deposit to create one. It can be used to store value without a wallet or to transfer value.</Typography>
-            </Stack>
-
-            <Stack sx={{ mt: 2, display: "flex", flexDirection: "row", justifyContent: "center" }}>
-                <TextField type="number" label="Deposit Amount" value={props.depositAmount} onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    props.setDepositAmount(event.target.value);
-                }}></TextField>
-
+                <Typography component="p" variant="subtitle1">Jetton Notes are financial claims for value that was deposited into a smart contract. Currently available for tgBTC. It can be used to store value without a wallet or to transfer value off-chain.</Typography>
             </Stack>
             <Stack sx={{ mt: 2, display: "flex", flexDirection: "row", justifyContent: "center" }}>
-                <Button variant="contained" onClick={() => props.createNoteClicked(props.depositAmount)}>Create Note</Button>                </Stack>
+                <Button variant="contained" onClick={() => props.createNoteClicked()}>Create Crypto Note</Button></Stack>
             <RouteFooter content="The Jetton Notes currently use tgBTC on Ton Testnet"></RouteFooter>
         </Paper>
     </Box >
