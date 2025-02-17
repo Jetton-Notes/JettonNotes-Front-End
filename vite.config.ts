1a9053784e2b1e64cac5e2708d35cd251f1bde87
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-02-18 00:44:09 +0100

    added topLevelAwait

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [wasm(), react(), nodePolyfills()],
+  plugins: [wasm(), react(), nodePolyfills(), topLevelAwait()],
   base: ((process.env.GITHUB_REPOSITORY ?? "") + "/").match(/(\/.*)/)?.[1],
 });
