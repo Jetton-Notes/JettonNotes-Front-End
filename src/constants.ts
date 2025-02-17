69f4d44486394c7defa07ddbe7ddbf3957f3bedd
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-02-17 15:26:37 +0100

    added TESTNET contract addresses

--- src/constants.ts
+++ src/constants.ts
@@ -4,8 +4,14 @@ export const website = "https://jettonnotes.com"
 
 export const botlink = "https://t.me/jetton_notes_bot"
 
-export const tgBTC_jetton_master = "JETTON MASTER ADDR PALCEHOLDER"
+export const tgBTC_jetton_master = "EQAtjC_IuHpCh65aReJ8NoSZ-_5EoI2JztuWKTq19ZQNfj3z";
+
+export const JETTONNOTES_ADDRESS = "EQD8MqMYqftZBllpavxsNyUBgltrD44H6WRKHiXUuDPSLvl9";
 
 export const currentNetwork = "TON testnet"
 
 export const onlyCURR = "tgBTC";
+
+export const SPLASHSCREENTIME = 2000;
+
+export const SPLASHANIMATIONTICK = 100;
