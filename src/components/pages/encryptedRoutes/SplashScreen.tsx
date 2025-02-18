bacf73ad0d4045accddc3febdb25d398a1165a2e
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-02-18 23:50:18 +0100

    added a loading indicator

--- src/components/pages/encryptedRoutes/SplashScreen.tsx
+++ src/components/pages/encryptedRoutes/SplashScreen.tsx
@@ -1,13 +1,15 @@
 import { Box, Typography, Paper, Stack } from "@mui/material";
 
-
 export function SplashScreenRoute() {
-    return <Box >
-        <Paper sx={{ maxWidth: 936, margin: "auto", overflow: "hidden", mt: "10px" }}>
+    return <Box sx={{ height: "100%" }} >
+        <Paper sx={{ height: "100%", maxWidth: 936, margin: "auto", overflow: "hidden", mt: "10px" }}>
             <Stack sx={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
-                <Typography component="h1" variant="h4">Welcome to Jetton Notes</Typography>
+                <Typography component="h1" variant="h4">Jetton Notes</Typography>
+
+            </Stack>
+            <Stack sx={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
+                <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
             </Stack>
         </Paper>
     </Box >
-
 }
