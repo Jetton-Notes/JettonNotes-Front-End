217c194e2196d8a8374b9f0a641e6e08dd6fa7e5
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-02-19 03:18:12 +0100

    created relayer snark generator

---
+++ src/actions/getRelayerSnark.ts
@@ -0,0 +1,61 @@
+import { decryptData, Status } from "../crypto/encrypt";
+import { getAccountKey } from "../storage";
+import { bip32derivedDeposit, generateNoteWithdrawProof, parseNote, toNoteHex } from "../crypto/cryptonotes";
+
+//This will get the zkSnark for the relayed transaction
+export async function getRelayerSnark(
+    transferTo: bigint,
+    amount: bigint,
+    currentUtxoIndex: number,
+    password: string
+) {
+    const workchain = 0n;
+    const recipient = 0n; // There don't matter because they are not checked during a relayed tx..
+
+    const accountKeyFound = await getAccountKey();
+    if (accountKeyFound) {
+        const masterKeyData = await decryptData(accountKeyFound.cipherText.buffer, password);
+
+        if (masterKeyData.status === Status.Success) {
+            const parsedNote = await parseNote(masterKeyData.data);
+            const currentNote = await bip32derivedDeposit({
+                masterSecret: parsedNote.deposit.secret,
+                masterNullifier: parsedNote.deposit.nullifier,
+                counter: currentUtxoIndex,
+            })
+            const utxoNote = await bip32derivedDeposit({
+                masterSecret: parsedNote.deposit.secret,
+                masterNullifier: parsedNote.deposit.nullifier,
+                counter: currentUtxoIndex + 1,
+            })
+
+            const { proof, publicSignals } = await generateNoteWithdrawProof({
+                deposit: currentNote,
+                recipient,
+                workchain,
+                transferto_commitment: transferTo,
+                transferto_amount: amount,
+                utxo_commitment: utxoNote.commitment,
+                snarkArtifacts: {
+                    wasmFilePath: "./circuit.wasm",
+                    zkeyFilePath: "./circuit_final.zkey"
+                }
+            });
+
+            return {
+                success: true,
+                snark: { proof, publicSignals },
+                error: ""
+            }
+        }
+
+
+    }
+
+    return {
+        success: false,
+        snark: {},
+        error: ""
+    }
+
+}
