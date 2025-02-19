e1f581efbf41ce380439da8ce92d347ece716c0c
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-02-19 03:17:30 +0100

    added new constants

--- src/constants.ts
+++ src/constants.ts
@@ -6,12 +6,17 @@ export const botlink = "https://t.me/jetton_notes_bot"
 
 export const tgBTC_jetton_master = "EQAtjC_IuHpCh65aReJ8NoSZ-_5EoI2JztuWKTq19ZQNfj3z";
 
-export const JETTONNOTES_ADDRESS = "EQD8MqMYqftZBllpavxsNyUBgltrD44H6WRKHiXUuDPSLvl9";
+export const JETTONNOTES_ADDRESS = "EQDhWIYpjuTbk8dzEGwst16pnSu8BuDA3NRkfHc7Yo8X6A92";
 
-export const currentNetwork = "TON testnet"
+export const currentNetwork = "testnet"
 
 export const onlyCURR = "tgBTC";
 
 export const SPLASHSCREENTIME = 2000;
 
 export const SPLASHANIMATIONTICK = 100;
+
+export const MainnetAPI = "https://toncenter.com/api/v2/jsonRPC"
+export const TestnetAPI = "https://testnet.toncenter.com/api/v2/jsonRPC"
+
+export const RelayerURL = "http://localhost:8000";
