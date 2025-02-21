23eb898bb07461a6de6da08976ed11c60249f948
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-02-21 02:11:14 +0100

    updated contract address

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ export const botlink = "https://t.me/jetton_notes_bot"
 
 export const tgBTC_jetton_master = "EQAtjC_IuHpCh65aReJ8NoSZ-_5EoI2JztuWKTq19ZQNfj3z";
 
-export const JETTONNOTES_ADDRESS = "EQDhWIYpjuTbk8dzEGwst16pnSu8BuDA3NRkfHc7Yo8X6A92";
+export const JETTONNOTES_ADDRESS = "EQBPd9iwc7sqBrvzSeVE5gzuZmGVaWaxSbYQtckCRXN6oI0i";
 
 export const currentNetwork = "testnet"
 
@@ -19,4 +19,4 @@ export const SPLASHANIMATIONTICK = 100;
 export const MainnetAPI = "https://toncenter.com/api/v2/jsonRPC"
 export const TestnetAPI = "https://testnet.toncenter.com/api/v2/jsonRPC"
 
-export const RelayerURL = "http://localhost:8000";
+export const RelayerURL = "http://localhost:3000/relayTransaction";
